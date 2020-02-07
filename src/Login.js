fd3e6aeacbc1458467a6bc4077b4f870ec53c814
Lucas Jacques <lucas.jacs@gmail.com>
2020-02-07 02:56:52 -0300

    Added create-react-app code + new Component to kickstart it

---
+++ src/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function Login() {
+  return (
+    <div className="Login">
+      <header className="Login-header">
+        <p>
+          Type your address and let's start cooking!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Login;
